f2b1ae36935f11e6b9844469f7e5a1e8b024a4b0
Damrocka89 <ew.krasowska@hotmail.com>
2019-01-30 08:29:32 +0100

    ArraysListContacts

--- src/com/company/ArrayLists/Contact.java
+++ src/com/company/ArrayLists/Contact.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.company.ArrayLists;
 
 public class Contact {
 
--- src/com/company/ArrayLists/Main.java
+++ src/com/company/ArrayLists/Main.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.company.ArrayLists;
 
 import java.util.Scanner;
 
